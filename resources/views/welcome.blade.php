a4c5128b9ee6dc278e7b0af5c9d56b85ee2b435a
Marcus <Marcus>
2017-06-02 10:29:12 +0200

    update  the  mobile  api

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Site made with Mobirise Website Builder v3.10.4, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v3.10.4, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-139x128-92.jpg" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;subset=latin">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
+
+       <link href="{{ asset('assets/bootstrap-material-design-font/css/material.css') }}" rel="stylesheet">
+     <link href="{{ asset('assets/web/assets/mobirise-icons/mobirise-icons.css') }}" rel="stylesheet">
+     <link href="{{ asset('assets/et-line-font-plugin/style.css') }}" rel="stylesheet">
+      <link href="{{ asset('assets/tether/tether.min.css') }}" rel="stylesheet">
+      <link href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+	  
+    <link href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('assets/animate.css/animate.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/dropdown/css/style.css') }}" rel="stylesheet">
+
+<link href="{{ asset('assets/theme/css/style.css') }}" rel="stylesheet">
+
+<link href="{{ asset('assets/mobirise/css/mbr-additional.css') }}" rel="stylesheet">
+  
+  
+  
+</head>
+<body>
+<section id="menu-0">
+
+    <nav class="navbar navbar-dropdown bg-color transparent navbar-fixed-top">
+        <div class="container">
+
+            <div class="mbr-table">
+                <div class="mbr-table-cell">
+
+                    <div class="navbar-brand">
+                        <a href="#" class="navbar-logo"><img src="assets/images/logo-139x128dsdsdsdfsdfssd-92.png" alt="Mobirise"></a>
+                        <a class="navbar-caption" href="#">ConnectU Media</a>
+                    </div>
+
+                </div>
+                <div class="mbr-table-cell">
+
+                    <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="hamburger-icon"></div>
+                    </button>
+
+                    <ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar"><li class="nav-item"><a class="nav-link link" href="home">HOME &nbsp;</a></li><li class="nav-item"><a class="nav-link link" href="coporate">CORPORATE&nbsp;</a></li><li class="nav-item"><a class="nav-link link" href="medical">MEDICAL &nbsp;PRACTICES&nbsp;</a></li><li class="nav-item"><a class="nav-link link" href="contactus">CONTACT &nbsp;US &nbsp;&nbsp;</a></li></ul>
+                    <button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="close-icon"></div>
+                    </button>
+
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+</section>
+
+<section class="engine"><a rel="external" href="https://mobirise.com">top website creator software download</a></section><section class="mbr-slider mbr-section mbr-section__container carousel slide mbr-section-nopadding mbr-after-navbar" data-ride="carousel" data-keyboard="false" data-wrap="true" data-pause="false" data-interval="5000" id="slider-2">
+    <div>
+        <div>
+            <div>
+                <ol class="carousel-indicators">
+                    <li data-app-prevent-settings="" data-target="#slider-2" data-slide-to="0" class="active"></li><li data-app-prevent-settings="" data-target="#slider-2" class="" data-slide-to="1"></li><li data-app-prevent-settings="" data-target="#slider-2" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="mbr-section mbr-section-hero carousel-item dark center mbr-section-full active" data-bg-video-slide="false" style="background-image: url(assets/images/pic-1-2000x1776-73.jpg);">
+                        <div class="mbr-table-cell">
+                            <div class="mbr-overlay" style="opacity: 0;"></div>
+                            <div class="container-slide container">
+                                
+                                <div class="row">
+                                    <div class="col-md-8 col-md-offset-2 text-xs-center">
+                                        <h2 class="mbr-section-title display-1">ConnectU &nbsp; Media&nbsp;</h2>
+                                        
+
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><div class="mbr-section mbr-section-hero carousel-item dark center mbr-section-full" data-bg-video-slide="false" style="background-image: url(assets/images/pic2-2000x1333-6.jpg);">
+                        <div class="mbr-table-cell">
+                            <div class="mbr-overlay" style="opacity: 0;"></div>
+                            <div class="container-slide container">
+                                
+                                <div class="row">
+                                    <div class="col-md-8 col-md-offset-1">
+                                        <h2 class="mbr-section-title display-1"></h2>
+                                        <p class="mbr-section-lead lead"></p>
+
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><div class="mbr-section mbr-section-hero carousel-item dark center mbr-section-full" data-bg-video-slide="false" style="background-image: url(assets/images/pic3-2000x1098-95.jpg);">
+                        <div class="mbr-table-cell">
+                            <div class="mbr-overlay" style="opacity: 0;"></div>
+                            <div class="container-slide container">
+                                
+                                <div class="row">
+                                    <div class="col-md-8 col-md-offset-3 text-xs-right">
+                                        
+                                        
+
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <a data-app-prevent-settings="" class="left carousel-control" role="button" data-slide="prev" href="#slider-2">
+                    <span class="icon-prev" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a data-app-prevent-settings="" class="right carousel-control" role="button" data-slide="next" href="#slider-2">
+                    <span class="icon-next" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section" id="msg-box5-8" style="background-color: rgb(255, 255, 255); padding-top: 0px; padding-bottom: 0px;">
+
+    
+    <div class="container">
+        <div class="row">
+            <div class="mbr-table-md-up">
+
+              
+
+              <div class="mbr-table-cell col-md-5 text-xs-center text-md-right content-size">
+                  <h3 class="mbr-section-title display-2">Who &nbsp; are &nbsp;we &nbsp; ?&nbsp;</h3>
+                  <div class="lead">
+
+                    <p><br>
+					<strong>ConnectU is a dynamic media company that provides a unique opportunity for medical companies which benefits the public, medical practices and practitioners as well. By medical companies advertising on the screens it gives them the chance to better its brand image, expand its brand awareness, give back to the community and target specific and related customers</strong>
+					<br><br><strong>What makes us so effective...?</strong><br><br>
+					<strong>As the screens are situated in medical rooms patients have a direct interest in the companies and their products. If a patient makes an appointment to visit a Health Professional, good manner requires that you be there 15 minutes’ prior. Due to the nature of their work, health care professionals may be behind schedule and may only see the patient 30 minutes after the initial appointed time. This means that the patient will wait for 45 minutes in the waiting room with nothing to do except look at the screen with varies adverts which relate directly to them</strong>.<br></p>
+
+                  </div>
+
+                  
+              </div>
+
+
+              
+
+
+              <div class="mbr-table-cell mbr-left-padding-md-up mbr-valign-top col-md-7 image-size" style="width: 50%;">
+                  <div class="mbr-figure"><img src="assets/images/old-tv-1400x787-46.jpg"></div>
+              </div>
+
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="mbr-cards mbr-section mbr-section-nopadding" id="features4-9" style="background-color: rgb(255, 255, 255);">
+
+    
+
+    <div class="mbr-cards-row row">
+        <div class="mbr-cards-col col-xs-12 col-lg-4" style="padding-top: 80px; padding-bottom: 80px;">
+            <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img iconbox"><a href="coporate" class="mbri-growing-chart mbr-iconfont mbr-iconfont-features4" style="color: black;"></a></div>
+                    <div class="card-block">
+                        <h4 class="card-title">Corporate</h4>
+                        <h5 class="card-subtitle"></h5>
+                        <p class="card-text"><strong>Why choose us? We Strive for perfection<br>and excellence and to be the leading marketing company<br> not only in&nbsp;South Africa but through out world.<br>Advertising with us provides numerous advantages....</strong></p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="mbr-cards-col col-xs-12 col-lg-4" style="padding-top: 80px; padding-bottom: 80px;">
+            <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img iconbox"><a href="medical" class="mbri-cloud mbr-iconfont mbr-iconfont-features4" style="color: black;"></a></div>
+                    <div class="card-block">
+                        <h4 class="card-title">Medical &nbsp;Practices &nbsp;</h4>
+                        <h5 class="card-subtitle"></h5>
+                        <p class="card-text"><strong>Whats In it for the medical Practices? We <br>believe that everyone should benefit in their own unique way and we offer many exciting benefits for the medical practices in particular......</strong></p>
+                        
+                    </div>
+                </div>
+          </div>
+        </div>
+        <div class="mbr-cards-col col-xs-12 col-lg-4" style="padding-top: 80px; padding-bottom: 80px;">
+            <div class="container">
+                <div class="card cart-block">
+                    <div class="card-img iconbox"><a href="contactus" class="etl-icon icon-phone mbr-iconfont mbr-iconfont-features4" style="color: black;"></a></div>
+                    <div class="card-block">
+                        <h4 class="card-title">Get &nbsp;in &nbsp;Touch &nbsp;</h4>
+                        <h5 class="card-subtitle"></h5>
+                        <p class="card-text"><strong>Contact us to find out more....</strong>.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+        
+    </div>
+</section>
+
+<section class="mbr-section mbr-section-md-padding mbr-footer footer1 mbr-parallax-background" id="contacts1-b" style="background-image: url(assets/images/jumbotron.jpg); padding-top: 60px; padding-bottom: 30px;">
+    <div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(60, 60, 60);"></div>
+    <div class="container">
+        <div class="row">
+            <div class="mbr-footer-content col-xs-12 col-md-3">
+                <div><img src="assets/images/logo-139x128dsdsdsdfsdfssd-92.png"></div>
+            </div>
+            <div class="mbr-footer-content col-xs-12 col-md-4">
+                <p>About us<br><br>ConnectU is a marketing company that specializes in the medical field.Our vision is to be the leading marketing company in Africa by installing digital screens all over the continent in renowned hospitals and medical practices.Our goal is to create a company that benefits all aspects of the Industry, ConnectU provides an opportunity for the general public, medical practices and multinational companies to benefit from each other in their own unique way. We pride our selves in the education and well being of others.<br></p>
+            </div>
+          
+            <div class="mbr-footer-content col-xs-12 col-md-3">
+                <p>Contacts<br><br>Email: greg@connectumedia.co.za<br>Phone: +27827982660<br><br></p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+
+  <script src="{{ asset('assets/web/assets/jquery/jquery.min.js') }}"></script> 
+  
+  <script src="{{ asset('assets/tether/tether.min.js') }}"></script> 
+
+  <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script> 
+ <script src="{{ asset('assets/smooth-scroll/SmoothScroll.js') }}"></script> 
+<script src="{{ asset('assets/viewportChecker/jquery.viewportchecker.js') }}"></script> 
+   <script src="{{ asset('assets/dropdown/js/script.min.js') }}"></script> 
+  
+  
+  <script src="{{ asset('assets/bootstrap-carousel-swipe/bootstrap-carousel-swipe.js') }}"></script> 
+  <script src="{{ asset('assets/touchSwipe/jquery.touchSwipe.min.js') }}"></script> 
+   <script src="{{ asset('assets/theme/js/script.js') }}"></script> 
+<script src="{{ asset('assets/jarallax/jarallax.js') }}"></script> 
+  <input name="animation" type="hidden">
+  </body>
+</html>
